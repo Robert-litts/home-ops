45d7f3cd1327d80514496e8849a51cbb2da91272
Robbie <79869161+Robert-litts@users.noreply.github.com>
2024-06-23 13:35:51 -0400

    Update providers.tf

--- k3s_cluster/master/providers.tf
+++ k3s_cluster/master/providers.tf
@@ -11,13 +11,13 @@ terraform {
 
 provider "proxmox" {
   # url is the hostname (FQDN if you have one) for the proxmox host you'd like to connect to to issue the commands. my proxmox host is 'prox-1u'. Add /api2/json at the end for the API
-  pm_api_url = $(var.proxmox_api_url)
+  pm_api_url = var.proxmox_api_url
 
   # api token id is in the form of: <username>@pam!<tokenId>
-  pm_api_token_id = $(var.proxmox_api_token_id)
+  pm_api_token_id = var.proxmox_api_token_id
 
   # this is the full secret wrapped in quotes. don't worry, I've already deleted this from my proxmox cluster by the time you read this post
-  pm_api_token_secret = $(var.proxmox_api_token_secret)
+  pm_api_token_secret = var.proxmox_api_token_secret
 
   # leave tls_insecure set to true unless you have your proxmox SSL certificate situation fully sorted out (if you do, you will know)
   pm_tls_insecure = true
